584393aa2ffed42d730e641286b64989b6a79125
Christos Sidiropoulos <dev@csidirop.de>
2024-12-11 13:40:52 +0000

    Call `get_view()` only once
    Signed-off-by: Christos Sidiropoulos <dev@csidirop.de>

--- views/admin/config-form.php
+++ views/admin/config-form.php
@@ -1,15 +1,19 @@
+<?php
+    $view = get_view();
+?>
+
 <h2><?= __('Configure Log Paths') ?></h2>
 
 <?php foreach ((array)json_decode(get_option('belogs_logPaths')) as $option => $path): ?>
     <div class="field">
         <div class="two columns alpha">
-            <?php echo get_view()->formLabel($option, $option); ?>
+            <?php echo $view->formLabel($option, $option); ?>
         </div>
         <div class="inputs five columns omega">
             <p class="explanation">
                 <?php echo __('Path to the file:'); ?>
             </p>
-            <?php echo get_view()->formText($option, $path); ?>
+            <?php echo $view->formText($option, $path); ?>
         </div>
     </div>
 <?php endforeach; ?>
@@ -17,7 +21,7 @@
 <h2><?= __('Configure Access Rights') ?></h2>
 <div class="field">
     <div class="two columns alpha">
-        <?php echo get_view()->formLabel('belogs-rolesACL', "Backend Logs Access"); ?>
+        <?php echo $view->formLabel('belogs-rolesACL', "Backend Logs Access"); ?>
     </div>
     <div class="inputs five columns omega">
         <p class="explanation"><?php echo __('Select userroles allowed to see the Logs'); ?></p>
@@ -37,10 +41,10 @@
                     foreach ($userRoles as $role=>$option) {
                         echo '<tr>';
                         echo '<td class="column-user">' . __($role) . '</td>';
-                        if($role != "super") {
-                            echo '<td class="boxes">' . get_view()->formCheckbox('belogs-rolesACL[]', $role, ['id' => 'belogs-rolesACL-'.$role.'-id'],  $option ? [$role] : []) . '</td>';
+                        if ($role != "super") {
+                            echo '<td class="boxes">' . $view->formCheckbox('belogs-rolesACL[]', $role, ['id' => 'belogs-rolesACL-'.$role.'-id'],  $option ? [$role] : []) . '</td>';
                         } else { //disable the checkbox for the super user
-                            echo '<td class="boxes">' . get_view()->formCheckbox('belogs-rolesACL[]', $role, ['id' => 'belogs-rolesACL-'.$role.'-id', 'disabled' => 'disabled'],  $option ? [$role] : []) . '</td>';
+                            echo '<td class="boxes">' . $view->formCheckbox('belogs-rolesACL[]', $role, ['id' => 'belogs-rolesACL-'.$role.'-id', 'disabled' => 'disabled'],  $option ? [$role] : []) . '</td>';
                         }
                         echo '</tr>';
                     }
